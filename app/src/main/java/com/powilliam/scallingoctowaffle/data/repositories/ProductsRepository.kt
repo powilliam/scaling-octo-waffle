dfecde949d5e886b5e034990d0d7ba5115ef7241
William Porto <powilliam19@gmail.com>
2024-02-19 13:44:29 -0300

    repositories for product and pricing

---
+++ app/src/main/java/com/powilliam/scallingoctowaffle/data/repositories/ProductsRepository.kt
@@ -0,0 +1,23 @@
+package com.powilliam.scallingoctowaffle.data.repositories
+
+import com.powilliam.scallingoctowaffle.data.entities.Product
+import com.powilliam.scallingoctowaffle.data.entities.ProductWithPricings
+import com.powilliam.scallingoctowaffle.data.sources.local.ProductsLocalDataSource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface ProductsRepository {
+    fun allWithPricings(): Flow<List<ProductWithPricings>>
+    suspend fun insert(product: Product)
+    suspend fun delete(product: Product)
+}
+
+class ProductsRepositoryImpl @Inject constructor(
+    private val dataSource: ProductsLocalDataSource
+) : ProductsRepository {
+    override fun allWithPricings() = dataSource.allWithPricings()
+
+    override suspend fun insert(product: Product) = dataSource.insert(product)
+
+    override suspend fun delete(product: Product) = dataSource.delete(product)
+}
